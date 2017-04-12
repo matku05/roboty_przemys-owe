2047242e043c9c1be37668b66a7eb3c0e07918a0
Mateusz <mate.kukla@gmai.lcom>
2017-04-12 13:25:32 +0200

    z serverem tcplisner

--- Manipulatory/Manipulatory/MainWindow.xaml.cs
+++ Manipulatory/Manipulatory/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ using System.Net.Sockets;
 
 
 
+
 namespace Manipulatory
 {
     /// <summary>
@@ -50,11 +51,7 @@ namespace Manipulatory
         public void zmienne()
         {
             Variables.ip_addr = CB_ip_adress.Text;
-            //  Console.WriteLine(Variables.ip_addr);
-            IPAddress ipAd = IPAddress.Parse(Variables.ip_addr);
-            //  IPAddress ipAd = IPAddress.Parse(Variables.ip_addr);
-            TcpListener myList = new TcpListener(ipAd, 8001);
-
+      
         }
         public SerialPort Sp;
         public delegate void NoArgDelegate();
@@ -138,53 +135,84 @@ namespace Manipulatory
         {
             Sp.Write("GO \r");
         }
-
+        TcpListener server = null;
         public  void bt_SERVER_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                Variables.ip_addr = CB_ip_adress.Text;
-                //  Console.WriteLine(Variables.ip_addr);
-                IPAddress ipAd = IPAddress.Parse(Variables.ip_addr);
-                //  IPAddress ipAd = IPAddress.Parse(Variables.ip_addr);
-                TcpListener myList = new TcpListener(ipAd, 8001);
-
-                myList.Start();
-                Socket s = myList.AcceptSocket();
-
-                byte[] b = new byte[100];
-                int k = s.Receive(b);
                 
-                tb_connect_android.Text = ("Recieved...");
-                for (int i = 0; i < k; i++)
-                    Console.Write(Convert.ToChar(b[i]));
+                // Set the TcpListener on port 13000.
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
 
-                ASCIIEncoding asen = new ASCIIEncoding();
-                s.Send(asen.GetBytes("The string was recieved by the server."));
-                Console.WriteLine("\nSent Acknowledgement");
-                tb_connect_android.Text = ("Recieved..." +  "Sent Acknowledgement");
-                /* clean up */
-                s.Close();
-                myList.Stop();
-                tb_connect_android.Text = "połączono";
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
 
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                // Enter the listening loop.
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also user server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Sent: {0}", data);
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
             }
-            catch (Exception )
+            catch (SocketException xe)
             {
-                Console.WriteLine("bląd"  );
-                tb_connect_android.Text = "nie połączono";
+                Console.WriteLine("SocketException: {0}", xe);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
             }
 
-           
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+
 
         }
 
         public void bt_server_stop_Click(object sender, RoutedEventArgs e)
         {
-            Socket s = myList.AcceptSocket();
-            s.Close();
-            myList.Stop();
-            tb_connect_android.Text = "połączono";
+            
         }
     }
 }
